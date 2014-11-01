66700e79ffaeb5d43ad5265856246066e8631c89
Al Viro <viro@zeniv.linux.org.uk>
2014-11-01 00:37:32 -0400

    BACKPORT: make proc_ns_operations work with struct ns_common * instead of void *
    We can do that now.  And kill ->inum(), while we are at it - all instances
    are identical.
    
    Change-Id: Ia9cf67a1857767aa5f8c99a14e66e44881d0baaa
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>

--- ipc/namespace.c
+++ ipc/namespace.c
@@ -150,7 +150,7 @@ void put_ipc_ns(struct ipc_namespace *ns)
 	}
 }
 
-static void *ipcns_get(struct task_struct *task)
+static struct ns_common *ipcns_get(struct task_struct *task)
 {
 	struct ipc_namespace *ns = NULL;
 	struct nsproxy *nsproxy;
@@ -164,12 +164,12 @@ static void *ipcns_get(struct task_struct *task)
 	return ns;
 }
 
-static void ipcns_put(void *ns)
+static void ipcns_put(struct ns_common *ns)
 {
 	return put_ipc_ns(ns);
 }
 
-static int ipcns_install(struct nsproxy *nsproxy, void *new)
+static int ipcns_install(struct nsproxy *nsproxy, struct ns_common *new)
 {
 	struct ipc_namespace *ns = new;
 	if (!ns_capable(ns->user_ns, CAP_SYS_ADMIN) ||
@@ -183,18 +183,10 @@ static int ipcns_install(struct nsproxy *nsproxy, void *new)
 	return 0;
 }
 
-static unsigned int ipcns_inum(void *vp)
-{
-	struct ipc_namespace *ns = vp;
-
-	return ns->ns.inum;
-}
-
 const struct proc_ns_operations ipcns_operations = {
 	.name		= "ipc",
 	.type		= CLONE_NEWIPC,
 	.get		= ipcns_get,
 	.put		= ipcns_put,
 	.install	= ipcns_install,
-	.inum		= ipcns_inum,
 };
