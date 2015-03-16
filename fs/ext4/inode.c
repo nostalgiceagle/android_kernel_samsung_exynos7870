738a1625572225ac97a2e94a3daa09506226c4cc
Omar Sandoval <osandov@osandov.com>
2015-03-16 04:33:51 -0700

    UPSTREAM: Remove rw from dax_{do_,}io()
    And use iov_iter_rw() instead.
    
    Change-Id: Id83fa248c44c14c0ef2e0f6dc0066644da4d0f2c
    Signed-off-by: Omar Sandoval <osandov@osandov.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    (cherry picked from commit a95cd6311512bd954e88684eb39373f7f4b0a984)
    Signed-off-by: nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -3241,7 +3241,7 @@ static ssize_t ext4_ext_direct_IO(int rw, struct kiocb *iocb,
 	BUG_ON(ext4_encrypted_inode(inode) && S_ISREG(inode->i_mode));
 #endif
 	if (IS_DAX(inode))
-		ret = dax_do_io(rw, iocb, inode, iter, offset, get_block_func,
+		ret = dax_do_io(iocb, inode, iter, offset, get_block_func,
 				ext4_end_io_dio, dio_flags);
 	else
 		ret = __blockdev_direct_IO(iocb, inode,
