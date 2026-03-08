9572fa6ed282ef63271b3a51ff926fee6c487cda
nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>
2026-03-08 20:32:51 +0000

    ok
    Change-Id: I798a4189302ef267320e5e52f17bc9294320bbfd
    Signed-off-by: nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=/media/astrako/DATOS/toolchain/gcc-linaro-7.5.0/bin/aarch64-linux-gnu-
+export CROSS_COMPILE=$(pwd)/toolchain/bin/aarch64-linux-android-
 export ANDROID_MAJOR_VERSION=q
 export ANDROID_PLATFORM_VERSION=10
 
